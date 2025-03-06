aaf0c9a2ce7b9de10ac3fb7747bd8a43fc5b426b
Zaman Sikander <sheikhzaman857@gamil.com>
2025-03-06 12:21:05 +0500

    how it work section complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const steps = [
     id: 1,
     title: "Browse UI Components",
     description: "Explore a collection of beautifully designed, ready-to-use UI components.",
-    icon: <Code size={30}/>,
+    icon: <Code size={30} />,
   },
   {
     id: 2,
@@ -60,54 +60,60 @@ const Home = () => {
       {/* category section */}
 
       <main className="py-12 min-h-screen">
-      <div className="max-w-6xl mx-auto px-4">
-        <h2 className="text-xl sm:text-2xl lg:text-3xl text-indigo-600 font-semibold font-headings text-center">Explore Our Component Categories</h2>
-        <p className="text-gray-600 text-center mb-12 max-w-2xl mx-auto font-body">
-          Discover a wide range of prebuilt components crafted with modern design and full customization support.
-        </p>
+        <div className="max-w-6xl mx-auto px-4">
+          <h2 className="text-xl sm:text-2xl lg:text-3xl text-indigo-600 font-semibold font-headings text-center">Explore Our Component Categories</h2>
+          <p className="text-gray-600 text-center mb-12 max-w-2xl mx-auto font-body">
+            Discover a wide range of prebuilt components crafted with modern design and full customization support.
+          </p>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-          {categories.map((category, index) => (
-            <div
-              key={index}
-              className="cursor-pointer p-6 rounded-2xl hover:bg-gray-50 shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center"
-            >
-              <div className="p-4 bg-indigo-100 text-indigo-600 rounded-full mb-4">{category.icon}</div>
-              <h3 className="text-base md:text-lg font-semibold text-gray-800">{category.title}</h3>
-              <p className="text-gray-600 mt-2 font-ui hidden md:block md:text-xs lg:text-sm">{category.description}</p>
-            </div>
-          ))}
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+            {categories.map((category, index) => (
+              <div
+                key={index}
+                className="relative p-6 sm:p-8 w-full bg-gradient-to-br from-indigo-500 to-indigo-600 rounded-2xl shadow-lg text-center text-white transition-transform transform hover:-translate-y-2 cursor-pointer"
+              >
+                <div className="mb-4 bg-white/20 w-fit mx-auto text-white rounded-full p-4">
+                  {category.icon}
+                </div>
+                <h3 className="text-sm sm:text-base  font-semibold">{category.title}</h3>
+                <p className="text-sm hidden sm:block md:text-base text-gray-200 py-2 md:block">{category.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
       </main>
 
       {/* how it works */}
 
-      <main className="mt-7 min-h-screen  py-12  flex flex-col justify-center items-center border gap-4 px-4 sm:px-6 md:px-8">
-      <div className="text-center">
-        <h2 className="text-xl sm:text-2xl lg:text-3xl text-indigo-600 font-semibold font-headings">
-          How It Works
-        </h2>
-        <p className="text-gray-600 mb-6 sm:mb-12 max-w-2xl mx-auto font-body">
-          🚀 No installations. No dependencies. Just plug & play!
-        </p>
-      </div>
-      
-      <div className="flex flex-col items-center py-6 sm:py-10 w-full">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-6xl px-4 sm:px-6 md:px-8">
-          {steps.map((step) => (
-            <div key={step.id} className="bg-white p-4 sm:p-6 rounded-2xl shadow-md hover:shadow-lg transition-all flex flex-col items-center text-center">
-              <div className="mb-4 bg-indigo-100 text-indigo-600 rounded-full p-4">{step.icon}</div>
-              <h3 className="text-lg sm:text-xl font-semibold text-gray-700 mb-2">{step.title}</h3>
-              <p className="text-gray-600 text-sm sm:text-base">{step.description}</p>
-            </div>
-          ))}
+      <main className="mt-7 min-h-screen  py-12  flex flex-col justify-center items-center gap-4 px-4 sm:px-6 md:px-8">
+        <div className="text-center">
+          <h2 className="text-xl sm:text-2xl lg:text-3xl text-indigo-600 font-semibold font-headings">
+            How It Works
+          </h2>
+          <p className="text-gray-600 mb-6 sm:mb-12 max-w-2xl mx-auto font-body">
+            🚀 No installations. No dependencies. Just plug & play!
+          </p>
         </div>
-        <button className="mt-6 sm:mt-8 bg-blue-500 text-white px-5 sm:px-6 py-2 sm:py-3 rounded-xl text-base sm:text-lg font-medium hover:bg-blue-600 transition-all">
-          🚀 Start Exploring
-        </button>
-      </div>
-    </main>
+
+        <div className="flex flex-col items-center py-6 sm:py-10 w-full">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-6xl px-4 sm:px-6 md:px-8">
+            {steps.map((step) => (
+              <div
+                className="relative p-6 sm:p-8 w-full max-w-sm bg-gradient-to-br from-indigo-500 to-indigo-600 rounded-2xl shadow-lg text-center text-white transition-transform transform hover:-translate-y-2 cursor-pointer"
+              >
+                <div className="mb-4 bg-white/20 w-fit text-white rounded-full p-4">
+                  {step.icon}
+                </div>
+                <h3 className="text-sm sm:text-base md:text-lg font-semibold">{step.title}</h3>
+                <p className="text-sm hidden  sm:text-base sm:block text-gray-200 py-2">{step.description}</p>
+              </div>
+            ))}
+          </div>
+          <button className="mt-7 px-6 py-3 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 cursor-pointer">
+            🚀 Start Exploring
+          </button>
+        </div>
+      </main>
     </div>
   )
 }
